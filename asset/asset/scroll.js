84bb02e783ae1af7a208e4cc2c52354c9eda6cab
Habbatul <gamernolifeneet@gmail.com>
2023-08-25 00:45:21 +0700

    styling css

--- asset/asset/scroll.js
+++ asset/asset/scroll.js
@@ -50,3 +50,79 @@ const observer4 = new IntersectionObserver((entries) => {
 
 const hiddenElements4 = document.querySelectorAll('.star');
 hiddenElements4.forEach((el) => observer4.observe(el));
+
+//animasi astroboy
+const MIN_SCREEN_WIDTH_TAB = 700;
+const MAX_SCREEN_WIDTH_TAB = 999;
+const MIN_SCREEN_WIDTH = 1000;
+const MAX_SCREEN_WIDTH = 1920;
+
+// Fungsi untuk membuat dan mengamati Intersection Observer dengan rootMargin
+function createAndObserveObserverWithRootMargin() {
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    },
+    {
+      rootMargin: '0px 0px -28% 0px', // Margin atas dikurangi 50px
+    }
+  );
+
+  const hiddenElements = document.querySelectorAll('.scrollAstro');
+  hiddenElements.forEach(el => observer.observe(el));
+}
+
+function createAndObserveObserverWithRootMarginTablet() {
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    },
+    {
+      rootMargin: '0px 0px -16% 0px', // Margin atas dikurangi 50px
+    }
+  );
+
+  const hiddenElements = document.querySelectorAll('.scrollAstro');
+  hiddenElements.forEach(el => observer.observe(el));
+}
+
+// Fungsi untuk membuat dan mengamati Intersection Observer tanpa rootMargin
+function createAndObserveObserverWithoutRootMargin() {
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    }
+  );
+
+  const hiddenElements = document.querySelectorAll('.scrollAstro');
+  hiddenElements.forEach(el => observer.observe(el));
+}
+
+// Periksa lebar layar dan pilih metode observasi yang sesuai
+const screenWidth = window.innerWidth;
+if (screenWidth >= MIN_SCREEN_WIDTH && screenWidth <= MAX_SCREEN_WIDTH) {
+  createAndObserveObserverWithRootMargin();
+} else if(screenWidth >= MIN_SCREEN_WIDTH_TAB  && screenWidth <= MAX_SCREEN_WIDTH_TAB ){
+  createAndObserveObserverWithRootMarginTablet();
+} 
+else {
+  createAndObserveObserverWithoutRootMargin();
+}
